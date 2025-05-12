1c7ef351bc9bceca2f7a308a2553817214f38128
Andrey Ivanov <this.andrey@gmail.com>
2025-05-12 17:13:09 +0300

    init

---
+++ tools/lake_cli/lib/lake.dart
@@ -0,0 +1,10 @@
+/// lake, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate lake
+/// dart pub global activate lake
+///
+/// # see usage
+/// lake --help
+/// ```
+library;
