0e1096aa4d9adb865219e2dd97591edf07d1631b
Andrey Ivanov <this.andrey@gmail.com>
2025-05-20 17:49:59 +0300

    tests: wip

---
+++ tools/lake_cli/test/src/utils/is_test.dart
@@ -0,0 +1,32 @@
+import 'package:lake_cli/src/utils/is.dart';
+import 'package:test/test.dart';
+
+import '../helpers/test_fs_helper.dart';
+
+void main() {
+  group('isFile', () {
+    late TestFsHelper fs;
+
+    setUp(() {
+      fs =
+          TestFsHelper()
+            ..createTree({'test_file.txt': '', 'test_directory': {}});
+    });
+
+    tearDown(() {
+      fs.cleanUp();
+    });
+
+    test('returns true for an existing file', () {
+      expect(isFile(fs.path('test_file.txt')), isTrue);
+    });
+
+    test('returns false for a directory', () {
+      expect(isFile(fs.path('test_directory')), isFalse);
+    });
+
+    test('throws ArgumentError for an empty path', () {
+      expect(() => isFile(''), throwsArgumentError);
+    });
+  });
+}
