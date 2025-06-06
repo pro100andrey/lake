6ffd7eac19382b6d9f6c39e7c4c3de6698a0f1a8
Andrii Ivanov <thsis.andrey@gmail.com>
2025-06-06 12:47:07 +0300

    test: wip

--- tools/lake_lang/test/ast/definitions/exception_definition_test.dart
+++ tools/lake_lang/test/ast/definitions/exception_definition_test.dart
@@ -37,11 +37,25 @@ void main() {
       final field1 = def.fields[0];
       final field2 = def.fields[1];
 
+      expect(field1.fieldId, isNull);
+
       expect((field1.type as BaseTypeNode).type, 'string');
+      expect(field1.type.span!.start.offset, 24);
+      expect(field1.type.span!.end.offset, 30);
+
       expect(field1.identifier.value, 'message');
+      expect(field1.identifier.span!.start.offset, 31);
+      expect(field1.identifier.span!.end.offset, 38);
+
+      expect(field2.fieldId, isNull);
 
       expect((field2.type as BaseTypeNode).type, 'i32');
+      expect(field2.type.span!.start.offset, 40);
+      expect(field2.type.span!.end.offset, 43);
+
       expect(field2.identifier.value, 'code');
+      expect(field2.identifier.span!.start.offset, 44);
+      expect(field2.identifier.span!.end.offset, 48);
 
       expect(
         def.span!.text,
@@ -49,6 +63,51 @@ void main() {
       );
       expect(def.span!.start.offset, 0);
       expect(def.span!.end.offset, 51);
+    });
+
+    test('should parse exception with fields with field index', () {
+      const source = 'exception MyException {1: string message; 2: i32 code; }';
+      final doc = parseAst(source);
+
+      expect(doc.definitions, hasLength(1));
+      final def = doc.definitions.first as ExceptionDefinitionNode;
+
+      expect(def.identifier.value, 'MyException');
+      expect(def.fields, hasLength(2));
+
+      final field1 = def.fields[0];
+      final field2 = def.fields[1];
+
+      expect(field1.fieldId!.value, '1');
+      expect(field1.fieldId!.span!.start.offset, 23);
+      expect(field1.fieldId!.span!.end.offset, 24);
+      
+      expect((field1.type as BaseTypeNode).type, 'string');
+      expect(field1.type.span!.start.offset, 26);
+      expect(field1.type.span!.end.offset, 32);
+
+      expect(field1.identifier.value, 'message');
+      expect(field1.identifier.span!.start.offset, 33);
+      expect(field1.identifier.span!.end.offset, 40);
+
+      expect(field2.fieldId!.value, '2');
+      expect(field2.fieldId!.span!.start.offset, 42);
+      expect(field2.fieldId!.span!.end.offset, 43);
+
+      expect((field2.type as BaseTypeNode).type, 'i32');
+      expect(field2.type.span!.start.offset, 45);
+      expect(field2.type.span!.end.offset, 48);
+      
+      expect(field2.identifier.value, 'code');
+      expect(field2.identifier.span!.start.offset, 49);
+      expect(field2.identifier.span!.end.offset, 53);
+
+      expect(
+        def.span!.text,
+        'exception MyException {1: string message; 2: i32 code; }',
+      );
+      expect(def.span!.start.offset, 0);
+      expect(def.span!.end.offset, 56);
     });
   });
 }
